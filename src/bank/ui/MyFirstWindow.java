fffc165cd5ee9f8cd816698c3bd0abb7f2866119
Pawel Rubach <pawel.rubach@sorcersoft.com>
2017-05-17 18:24:27 +0200

    Transfer GUI

--- src/bank/ui/MyFirstWindow.java
+++ src/bank/ui/MyFirstWindow.java
@@ -4,6 +4,8 @@ import oracle.jrockit.jfr.JFR;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by prubac on 5/17/2017.
@@ -31,12 +33,26 @@ public class MyFirstWindow {
         JTextField lastNameField = new JTextField(20);
         lastNamePanel.add(lastNameField);
 
+        lastNameField.setText("abc");
+        //lastNameField.setEnabled(false);
+
         JButton myButton = new JButton("Login");
-        panel1.add(myButton);
+
+        // Anonymous inner class
+        myButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println(lastNameField.getText());
+                JOptionPane.showMessageDialog(frame,
+                        "Hello " + firstNameField.getText() + " "
+                                + lastNameField.getText());
+            }
+        });
 
 
-        frame.getContentPane().add(panel1);
-        frame.getContentPane().add(lastNamePanel);
+        frame.add(panel1);
+        frame.add(lastNamePanel);
+        frame.add(myButton);
         frame.setVisible(true);
 
         JFrame frame2 = new JFrame();
